4bec891c46d1373910927758a170d91614572873
Akashiutchiha <christianhonore2003@gmail.com>
2023-02-28 22:12:22 +0100

    feat: A small Calculator using dart

--- bin/cli.dart
+++ bin/cli.dart
@@ -2,7 +2,29 @@ import 'package:cli/cli.dart';
 import 'dart:io';
 
 void main() {
-  Calculator calc = Calculator(Color: 'red');
-  int answer = calc.add(1, 2);
-  print('${answer} Calculated with ${calc.Color}');
+  //Create a calculator app with a user interface
+  Calculator calc = Calculator();
+  print('Welcome to the calculator app');
+  print('Enter the first number');
+  int num1 = int.parse(stdin.readLineSync()!);
+  print('Enter the second number');
+  int num2 = int.parse(stdin.readLineSync()!);
+  print('Enter the operation you want to perform(+, -, *, /)');
+  String operation = stdin.readLineSync()!;
+  switch (operation) {
+    case '+':
+      print(calc.add(num1, num2));
+      break;
+    case '-':
+      print(calc.subtract(num1, num2));
+      break;
+    case '*':
+      print(calc.multiply(num1, num2));
+      break;
+    case '/':
+      print(calc.divide(num1, num2));
+      break;
+    default:
+      print('Invalid operation');
+  }
 }
