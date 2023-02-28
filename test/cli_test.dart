26d079efca701944fe6b2485bd7f152d2ab90d63
Akashiutchiha <christianhonore2003@gmail.com>
2023-02-28 22:19:29 +0100

    fix: Performed all the test on functioins

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -1,8 +1,15 @@
+import 'dart:math';
+
 import 'package:cli/cli.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Calculator', () {
+    var a;
+    var b;
+    expect(Calculator().add(a, b), a + b);
+    expect(Calculator().subtract(a, b), a - b);
+    expect(Calculator().divide(a, b), a / b);
+    expect(Calculator().multiply(a, b), a * b);
   });
 }
